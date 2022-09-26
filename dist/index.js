8c6fe0208a011eee1d366818accd9921ee39c344
MasriNaser <Naser.Masri@cronos.be>
2022-09-26 12:03:39 +0200

    add object

--- dist/index.js
+++ dist/index.js
@@ -30,3 +30,52 @@ const isCompeleted= arr.filter(function(i){
   return i.text
 })
 console.log(isCompeleted)
+
+//
+const add = (n1 = 1, n2 = 1) => n1 + n2;
+
+console.log(add(5, 5));
+
+var myObject = { id: 1, text: "this is", isComplete: true };
+
+Object.keys(myObject).reduce(function (key, index) {
+    myObject[key];
+});
+
+console.log(myObject);
+
+const object1 = {
+    id: 1,
+    text: "this is a text",
+    isCompeleted: true,
+};
+
+for (const [key, value] of Object.entries(object1)) {
+
+    console.log(`${key}: ${value}`);
+}
+//constraction
+
+function Car(model, color, create) {
+    this.model = model;
+    this.color = color;
+    this.create = new Date(create);
+    this.getFullYear = function () {
+        return this.create.getFullYear();
+    }
+    this.getFullType = function () {
+        return `${this.model} ${this.color}`
+    }
+
+}
+
+const car1 = new Car("Opel", "Red", "12-3-2020");
+const car2 = new Car("Toyota", "Green", "3-05-1983")
+
+console.log(car1.create.getFullYear());
+
+console.log("fullYear: ", car2.getFullYear());
+
+console.log("fullType: ", car1.getFullType());
+
+
